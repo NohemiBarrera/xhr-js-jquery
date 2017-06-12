32f3faedaa0985270ce40fb832d0e806e43cc93c
Nohemi Barrera <nohemi.baol@gmail.com>
2017-06-12 16:48:50 -0500

    mostrando imagenes

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,80 @@
-$.getJSON("http://pokeapi.co/api/v2/pokemon/", function(response){
+
+
+var plantillaPokes = '<div data-url="**url**" data-img="**imgPokes**" class="miau card">' +
+		'<a href="#modalPokes">' +
+			'<img src="**direccionImg**"">' +
+			'<span>**nombrePoke**</span>' +
+		'</a>' +
+	'</div>';
+
+var imagenes = [
+'img/bulbasaur.png', 'img/ivysaur.png', 'img/venusaur.png', 'img/charmander.png', 
+'img/charmeleon.png', 'img/charizard.png', 'img/squirtle.png', 'img/wartortle.png',
+'img/blastoise.png', 'img/caterpie.png', 'img/metapod.png', 'img/butterfree.png', 
+'img/weedle.png', 'img/kakuna.png', 'img/beedrill.png', 'img/pidgey.png',
+'img/pidgeotto.png', 'img/pidgeot.png', 'img/rattata.png', 'img/raticate.png'
+];
+
+var plantillaModal = 
+    '<div class="modal-content">' +
+      '<h4>Detalles: </h4>' +
+      '<img src="**imagenPokes**">' +
+      '<p>Color:**color**</p>' +
+      '<p>Habitat:**habitat**</p>' +
+      '<p>Generación:**generacion**</p>' +
+    '</div>' +
+  '</div>';
+
+var cargarPagina = function(){
+	var url = 'http://pokeapi.co/api/v2/pokemon/';
+$.getJSON(url, function(response){
 	var pokemons = response.results;
 	crearPokemons(pokemons);
-});
-
-//var xhr = new XMLHttpRequest();
-//tiene dos métodos que nos sirven para validar el JSON:
-
-/*xhr.onreadystatechange = function (e) {
-	if (this.readyState === 4) {
-		if (this.status === 200) {
-			var response = JSON.parse(this.response);
-			var pokemons = response.results;
-
-			crearPokemons(pokemons);*/
-			/*var squads = JSON.parse(this.response);	
-			crearSquads(squads);*/
-	//	}
-		
-//	}
-//};
-
-/*xhr.open("GET", "http://pokeapi.co/api/v2/pokemon/");
-
-xhr.send();*/
-
-function crearPokemons(pokemons) {
-	var ul = document.getElementById("pokemons");
-
-	pokemons.forEach(function (pokemon) {
-		var image = document.createElement("img");
-		var li = document.createElement("li");
-		li.textContent = pokemon.name;
-		image.src = pokemon.source;
-		ul.appendChild(image);
-		ul.appendChild(li);
+	$('.modal').modal();
+	$('.nuevosPokes').click(obtenerDetalles);
 	});
-}
+};
+
+var obtenerDetalles = function (pokemon, imagen){
+	var nombre = pokemon.name[0].name;
+	var color = pokemon.color.name;
+	var habitat = pokemon.habitat.name;
+	var generacion = pokemon.generation.name;
+	var plantillaFinal = "";
+	plantillaFinal += plantillaModal.replace('**nombrePoke**', nombre)
+	.replace('**color**', color)
+	.replace('**habitat**', habitat)
+	.replace('**generacion**', generacion)
+	.replace('**imagenPokes**', imagen);
+	$('#modalPokes').html(plantillaFinal);
+};
+
+var masInfo = function(){
+	var urlEspecie = $(this).data('url').replace('pokemon', 'pokemon-species')
+	.replace("http://", 'https://');
+	var imagen = $(this).data("img");
+	$.getJSON(urlEspecie, function(response){
+		obtenerDetalles(response, imagen);
+	});
+};
+
+var crearPokemons = function(pokemons){
+	plantillaFinal = "";
+	pokemons.forEach(function(pokemon, z){
+		plantillaFinal += plantillaPokes.replace("**nombrePoke**", pokemon.name)
+		.replace("**direccionImg**", imagenes[z])
+		.replace("**url**", pokemon.url)
+		.replace("**imgPokes**", imagenes[z]);
+	});
+	//$('#pokedex').html(plantillaFinal);
+};
+
+
+
+
+$(document).ready(cargarPagina);
+
+
+
+
 
